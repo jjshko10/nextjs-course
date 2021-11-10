0f841a78ab8519f927259f12a4c7e3b84cb1c26f
Stanislav Lukyanenko <stanislav.lukyanenko@aiolane.com>
2021-11-10 11:36:27 +0300

    routing

---
+++ next-js/pages/404.js
@@ -0,0 +1,8 @@
+const Error = () => (
+    <>
+      <h1>404</h1>
+      <p>Something wrong</p>
+    </>
+  );
+    
+export default Error;
---
+++ next-js/pages/posts.js
@@ -0,0 +1,8 @@
+const Posts = () => (
+  <>
+    <h1>Posts list:</h1>
+    <p>Some text</p>
+  </>
+);
+  
+export default Posts;
